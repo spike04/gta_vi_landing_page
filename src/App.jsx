b44e4b3560e6ef5e7e30026a1dc40a41ae4821e0
spike04 <rubinbajracharya@yahoo.com>
2025-08-08 22:09:38 +0545

    Chor: Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
+
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1>Welcome to the world of GTA and GSAP!</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
